bad1cdacb8d63b8541272048480b45b8a2e7f9ab
Delcio Polanco <delciopolanco@gmail.com>
2023-10-30 01:10:53 -0400

    adding auth guard

---
+++ src/app/core/guards/auth-guard.ts
@@ -0,0 +1,21 @@
+import { Injectable, inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services';
+import { PATHS } from 'src/app/shared/utils';
+
+@Injectable()
+export class AuthGuardService {
+  constructor(public auth: AuthService, public router: Router) {}
+
+  canActivate(): boolean {
+    if (!this.auth.isAuthenticated()) {
+      this.router.navigate([PATHS.LOGIN]);
+      return false;
+    }
+    return true;
+  }
+}
+
+export const IsAuthenticated: CanActivateFn = (): boolean => {
+  return inject(AuthGuardService).canActivate();
+};

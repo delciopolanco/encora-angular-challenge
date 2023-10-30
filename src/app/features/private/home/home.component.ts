6bb04cf6107faf8156d7a87bc48cdfea043461e3
Delcio Polanco <delciopolanco@gmail.com>
2023-10-30 02:49:52 -0400

    fetching post in home component

--- src/app/features/private/home/home.component.ts
+++ src/app/features/private/home/home.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Post } from 'src/app/core/models/post';
+import { PostService } from './services/post.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  postList: Post[] = [];
+
+  constructor(private postService: PostService) {}
+
+  async ngOnInit() {
+    this.postList = await this.postService.getPosts();
+  }
+}

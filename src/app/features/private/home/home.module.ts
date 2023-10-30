b46c3bd5ad20430f56e1345653e46af22fcf9241
Delcio Polanco <delciopolanco@gmail.com>
2023-10-30 02:15:28 -0400

    creating post component

--- src/app/features/private/home/home.module.ts
+++ src/app/features/private/home/home.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './home.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { PostComponent } from './components/post/post.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, PostComponent],
   imports: [
     SharedModule,
     RouterModule.forChild([{ path: '', component: HomeComponent }]),
